f7704d84f6b6596af5ff15dfdc962ab2b51547ae
Jerry Ma <jerry.ma.nk@gmail.com>
2019-12-16 11:53:46 -0500

    update thermometry

--- tolteca/web/frontend/pages/thermometry.py
+++ tolteca/web/frontend/pages/thermometry.py
@@ -19,12 +19,15 @@ from functools import lru_cache
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import numpy as np
+import datetime
 
 
 app = get_current_dash_app()
 logger = get_logger()
+ctx = 'thermometry-graph'
 
-UPDATE_INTERVAL = 5000  # ms
+
+UPDATE_INTERVAL = 1000  # ms
 
 
 src = {
@@ -77,51 +80,102 @@ class Thermetry(NcScope):
         return cls.from_filepath(Path(link).resolve())
 
 
+fig_layout = dict(
+    uirevision=True,
+    yaxis={
+        'type': 'log',
+        'autorange': True,
+        'title': 'Temperature (K)'
+        },
+    xaxis={
+        'title': 'UT'
+        },
+    )
+
+
 def get_layout(**kwargs):
+    controls = html.Div([
+        dcc.Checklist(
+            id=f'{ctx}-control-toggles',
+            options=[
+                {'label': 'Collate', 'value': 'collate'},
+            ],
+            value=['collate', ]
+            ),
+        ])
     graph_view = html.Div([
         dcc.Interval(
-            id='thermometry-graph-update-timer',
+            id=f'{ctx}-update-timer',
             interval=UPDATE_INTERVAL),
-        dcc.Graph(id='thermometry-graph')
+        dcc.Graph(
+            id=f'{ctx}',
+            figure=dict(
+                data=get_traces(),
+                layout=dict(
+                    height=1000,
+                    **fig_layout
+                    )),
+            animate=True,
+            )
         ])
     return html.Div([
         dbc.Row([dbc.Col(html.H1(src['title'])), ]),
-        dbc.Row([dbc.Col(html.Div(id='entry-updated')), ]),
+        dbc.Row([dbc.Col(controls), ]),
         dbc.Row([dbc.Col(graph_view), ]),
         ])
 
 
-@timeit
-@app.callback([
-        Output('entry-updated', 'children'),
-        Output('thermometry-graph', 'figure')
-        ], [
-        Input('thermometry-graph-update-timer', 'n_intervals')], [
-        ])
-def entry_update(n_intervals):
+def get_traces():
     tm = Thermetry.from_link(src['runtime_link'])
-
-    print(tm.n_channels)
-    # tm.get_data(timeslice=[-12 * u.hours, None])
-
-    fig = make_subplots(
-        rows=tm.n_channels, cols=1,
-        subplot_titles=tm.channel_labels)
-    # fig['layout']['margin'] = {
-    #     'l': 30, 'r': 10, 'b': 30, 't': 10
-    # }
-    # fig['layout']['height'] = 1000
-    fig.update_layout(
-            height=400 * tm.n_channels, width=800, title_text="Thermometry")
-
-    # fig['layout']['legend'] = {'x': 0, 'y': 1, 'xanchor': 'left'}
     n_times = 100
-    for i in range(0, tm.n_channels):
-        fig.append_trace({
+    result = []
+    for i in range(tm.n_channels):
+        result.append({
             'x': np.asarray(tm.v_time(i)[-n_times:], dtype='datetime64[s]'),
             'y': tm.v_temp(i)[-n_times:],
             'name': tm.channel_labels[i],
             'mode': 'lines+markers',
             'type': 'scatter'
-        }, i + 1, 1)
-    return html.Div("Updated"), fig
+        })
+    time_latest = np.max([t['x'][-1] for t in result])
+    for t in result:
+        mask = np.where(
+                (t['x'] >= (time_latest - np.timedelta64(24, 'h'))) &
+                (t['y'] > 0.))[0]
+        t['x'] = t['x'][mask]
+        t['y'] = t['y'][mask]
+    return result
+
+
+@timeit
+@app.callback([
+        Output(f'{ctx}', 'figure')
+        ], [
+        Input(f'{ctx}-update-timer', 'n_intervals')], [
+        State(f'{ctx}-control-toggles', 'value')
+        ])
+def entry_update(n_intervals, control_toggles):
+    traces = get_traces()
+    if 'collate' in control_toggles:
+        n_panels = 1
+        fig_height = 900
+        fig_kwargs = dict()
+    else:
+        n_panels = len(traces)
+        fig_height = 300 * n_panels
+        fig_kwargs = dict(subplot_titles=[t['name'] for t in traces])
+
+    fig = make_subplots(
+            rows=n_panels, cols=1, **fig_kwargs)
+
+    fig.update_layout(
+            height=fig_height,
+            **fig_layout)
+    for i, t in enumerate(traces):
+        if 'collate' in control_toggles:
+            row = 1
+        else:
+            row = i + 1
+        col = 1
+        fig.append_trace(t, row, col)
+    return fig,
