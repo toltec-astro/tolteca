71efacceb61bfc7bf985b934fe95cdd399b01092
toltec <toltec@clipo>
2020-08-21 20:18:11 +0000

    fix permission error for autodrive plot

--- tolteca/scripts/kids_bin/plot_confirm.sh
+++ tolteca/scripts/kids_bin/plot_confirm.sh
@@ -5,5 +5,5 @@ after=$2
 for i in $before $after; do
     obsid=$(printf "%06d" ${i})
     python ${HOME}/toltec_astro/tolteca/tolteca/recipes/autodrive.py collect \
-        /data/data_toltec/reduced/toltec*_${obsid}_autodrive.a_drv -fo /dev/null --plot &
+        /data/data_toltec/reduced/toltec*_${obsid}_autodrive.a_drv -fo dummy_output --plot &
 done
