d3b29c88db3520fb108f9f056680bdea81eb85d3
toltec <toltec@taco>
2021-01-20 14:27:28 +0000

    minor fix

--- tolteca/web/__init__.py
+++ tolteca/web/__init__.py
@@ -111,6 +111,12 @@ dasha_config.update(
                     'title_icon': 'fas fa-tachometer-alt',
                 },
                 {
+                    "template": "tolteca.web.templates.userlog",
+                    "route_name": "userlog",
+                    "title_text": 'User Log',
+                    'title_icon': 'far fa-sticky-note',
+                },
+                {
                     "template": "tolteca.web.templates.logview",
                     "route_name": "logview",
                     "title_text": 'Log View',
