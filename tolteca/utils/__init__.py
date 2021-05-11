c7175557df01a90d402032a23c69760571a9bb00
Jerry Ma <jerry.ma.nk@gmail.com>
2021-05-10 23:49:54 -0400

    separate out runtime context base to tollan

--- tolteca/utils/__init__.py
+++ tolteca/utils/__init__.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python
 
-from tollan.utils.sys import touch_file
 from tollan.utils.fmt import pformat_yaml
 from tollan.utils import rupdate
-from tollan.utils.log import get_logger, logit
+from tollan.utils.log import get_logger
+from tollan.utils.dirconf import DirConfError, DirConfMixin
 
 from ..version import version
 from astropy.time import Time
-import astropy.units as u
-from yaml.dumper import SafeDumper
 import appdirs
-from pathlib import Path, PosixPath
-from datetime import datetime
+from pathlib import Path
 from cached_property import cached_property
-from schema import Schema, Use, Optional
+from schema import Use, Optional, Schema
 from copy import deepcopy
-import inspect
 import yaml
-import re
-import os
 
 
 def get_pkg_data_path():
@@ -30,24 +24,47 @@ def get_user_data_dir():
     return Path(appdirs.user_data_dir('tolteca', 'toltec'))
 
 
-class RuntimeContextError(Exception):
+class RuntimeContextError(DirConfError):
     """Raise when errors occur in `RuntimeContext`."""
     pass
 
 
-class RuntimeContext(object):
-    """A class that holds runtime context for pipeline.
+class RuntimeContext(DirConfMixin):
+    """A class to manage runtime contexts.
+
+    This class manages a set of configurations in a coherent way, providing
+    per-project persistence for user settings.
+
+    A runtime context can be constructed either from file system,
+    using the `tollan.utils.dirconf.DirConfMixin` under the hood, or
+    be constructed directly from a configuration dict composed
+    programatically. Property :attr:``is_persistent`` is set to True in the
+    former case.
 
     """
 
-    _file_contents = {
-            'logdir': 'log',
-            'bindir': 'bin',
-            'caldir': 'cal',
-            'setup_file': '50_setup.yaml'
-            }
-    _backup_items = ['setup_file', ]
-    _backup_time_fmt = "%Y%m%dT%H%M%S"
+    _contents = {
+        'bindir': {
+            'path': 'bin',
+            'type': 'dir',
+            'backup_enabled': False
+            },
+        'caldir': {
+            'path': 'cal',
+            'type': 'dir',
+            'backup_enabled': False
+            },
+        'logdir': {
+            'path': 'log',
+            'type': 'dir',
+            'backup_enabled': False
+            },
+        'setup_file': {
+            'path': '50_setup.yaml',
+            'type': 'file',
+            'backup_enabled': True
+            },
+        }
 
     logger = get_logger()
 
@@ -55,126 +72,85 @@ class RuntimeContext(object):
         if sum([rootpath is None, config is None]) != 1:
             raise RuntimeContextError(
                     "one and only one of rootpath and config has to be set")
+        if rootpath is not None:
+            # we expect that rootpath is already setup if constructed
+            # this way.
+            try:
+                rootpath = self.populate_dir(
+                        rootpath,
+                        create=False, force=True)
+            except DirConfError:
+                raise RuntimeContextError(
+                        f'missing runtime context contents in {rootpath}. '
+                        f'Use {self.__class__.__name__}.from_dir '
+                        f'with create=True instead.'
+                        )
+        self._rootpath = rootpath
+        # we delay the validating of config to accessing time
+        # in property ``config``
+        self._config = config
 
-        self._rootpath = self._normalize_rootpath(rootpath)
-        self._config = self.validate_config(config)
+    @property
+    def is_persistent(self):
+        """True if this context is created from a valid rootpath."""
+        return self._rootpath is not None
 
     @property
     def rootpath(self):
-        if self.is_dirty:
-            return self._config['runtime']['rootpath']
-        return self._rootpath
-
-    @property
-    def is_dirty(self):
-        """True if this context is not in sync with the file system."""
-        if self._rootpath is None:
-            return True
-        return False
-
-    @staticmethod
-    def _normalize_rootpath(rootpath):
-        if rootpath is None:
-            return None
-        if isinstance(rootpath, str):
-            rootpath = Path(rootpath)
-        return rootpath.resolve()
-
-    def __repr__(self):
-        if self.is_dirty:
-            return f"{self.__class__.__name__}(dirty, {self.rootpath})"
-        return f"{self.__class__.__name__}({self.rootpath})"
+        if self.is_persistent:
+            return self._rootpath
+        # the config runtime should always be available,
+        # since we add that at the end of the config property getter
+        return self.config['runtime']['rootpath']
 
     def __getattr__(self, name, *args):
-        if name in self._file_contents:
-            return self._get_content_path(self.rootpath, name)
+        # in case the config is not persistent,
+        # we return the content paths from the runtime dict
+        # make available the content attributes
+        if self.is_persistent:
+            return super().__getattr__(name, *args)
+        if name in self._contents.keys():
+            return self.config['runtime'][name]
         return super().__getattribute__(name, *args)
 
-    @classmethod
-    def _get_content_path(cls, rootpath, item):
-        if rootpath is None:
-            return None
-        return rootpath.joinpath(cls._file_contents[item])
+    def __repr__(self):
+        if self.is_persistent:
+            return f"{self.__class__.__name__}({self.rootpath})"
+        # an extra star to indication is not persistent
+        return f"{self.__class__.__name__}(*{self.rootpath})"
 
     @property
     def config_files(self):
-        """The list of config files present in the :attr:`config_files` if set.
+        """The list of config files present in the :attr:`config_files`.
 
-        Files with names match ``^\\d+_.+\\.ya?ml$`` in the :attr:`rootpath`
-        are returned.
+        Returns ``None`` if the runtime context is not persistent.
+
         """
-        if self.rootpath is None:
-            return None
-        return sorted(filter(
-            lambda p: re.match(r'^\d+_.+\.ya?ml$', p.name),
-            self.rootpath.iterdir()))
-
-    def to_dict(self):
-        """Return a dict representation of the runtime context."""
-        return {
-                attr: getattr(self, attr)
-                for attr in [
-                    'rootpath',
-                    ] + list(self._file_contents.keys())
-                }
+        if self.is_persistent:
+            return self.collect_config_files()
+        return None
 
     @cached_property
     def config(self):
-        """The config dict, created by merging all :attr:`config_files` if
-        set, or the dict passed to the constructor.
+        """The runtime context dict.
 
         """
-        if self.is_dirty:
-            cfg = self._config
+        if self.is_persistent:
+            cfg = self.collect_config_from_files(
+                    self.config_files, validate=True
+                    )
+            # update runtime info
+            cfg['runtime'] = self.to_dict()
         else:
-            config_files = self.config_files
-            self.logger.debug(
-                    f"load config from files: {pformat_yaml(config_files)}")
-            if len(config_files) == 0:
-                raise RuntimeContextError('no config file found.')
-            cfg = dict()
-            for f in self.config_files:
-                with open(f, 'r') as fo:
-                    c = yaml.safe_load(fo)
-                    if c is None:
-                        c = dict()  # allow empty yaml file
-                    if not isinstance(c, dict):
-                        # error if invalid config found
-                        raise RuntimeContextError(
-                                f"invalid config file {f}."
-                                f" No top level dict found.")
-                    rupdate(cfg, c)
-        cfg = self.validate_config(cfg)
-        # update runtime info
-        cfg['runtime'] = self.to_dict()
+            cfg = self.validate_config(self._config)
+            # here we also add the runtime dict if it not already exists
+            if 'runtime' not in cfg:
+                cfg['runtime'] = {
+                        attr: None
+                        for attr in self._get_to_dict_attrs()
+                        }
         self.logger.debug(f"loaded config: {pformat_yaml(cfg)}")
         return cfg
-
-    @classmethod
-    def validate_config(cls, cfg):
-        if cfg is None:
-            return None
-        return cls.get_config_schema().validate(cfg)
-
-    @classmethod
-    def get_config_schema(cls):
-        """Return a `schema.Schema` object that validates the config dict.
-
-        This combines all :attr:`_config_schema` defined in all
-        base classes.
-        """
-        # merge schema
-        d = dict()
-        for base in reversed(inspect.getmro(cls)):
-            if not hasattr(base, 'extend_config_schema'):
-                continue
-            s = base.extend_config_schema()
-            if s is None:
-                continue
-            if isinstance(s, Schema):
-                s = s.schema
-            rupdate(d, s)
-        return Schema(d)
 
     @classmethod
     def extend_config_schema(cls):
@@ -203,132 +179,55 @@ class RuntimeContext(object):
             Optional(object): object
             }
 
-    @cached_property
-    def yaml_dumper(self):
-
-        class yaml_dumper(SafeDumper):
-            """Yaml dumper that handles some additional types."""
-            pass
-
-        yaml_dumper.add_representer(
-                PosixPath, lambda s, p: s.represent_str(p.as_posix()))
-        yaml_dumper.add_representer(
-                u.Quantity, lambda s, q: s.represent_str(q.to_string()))
-        return yaml_dumper
-
-    @classmethod
-    def _create_backup(cls, path, dry_run=False):
-        timestamp = datetime.fromtimestamp(
-            path.lstat().st_mtime).strftime(
-                cls._backup_time_fmt)
-        backup_path = path.with_name(
-                f"{path.name}.{timestamp}"
-                )
-        with logit(cls.logger.info, f"backup {path} -> {backup_path}"):
-            if not dry_run:
-                os.rename(path, backup_path)
-        return backup_path
-
     @classmethod
     def from_dir(
-            cls, dirpath,
-            create=False, force=False, overwrite=False, dry_run=False
+            cls, dirpath, **kwargs
             ):
         """
         Create `RuntimeContext` instance from `dirpath`.
+
+        This is the preferred method to construct `RuntimeContext`
+        from arbitrary path.
+
+        For paths that have already setup previous as runtime context,
+        use the constructor instead.
 
         Parameters
         ----------
         dirpath : `pathlib.Path`, str
             The path to the work directory.
-
-        create : bool
-            When set to False, raise `RuntimeContextError` if `path` does not
-            already have all content items. Otherwise, create the missing ones.
-
-        force : bool
-            When False, raise `RuntimeContextError` if `dirpath` is not empty
-
-        overwrite : bool
-            When False, backups for existing files is created.
-
-        dry_run : bool
-            If True, no actual file system changed is made.
-
-        kwargs : dict
-            Keyword arguments passed directly into the created
-            config file.
+        **kwargs : dict, optional
+            Additional arguments passed to the underlying
+            :meth:`DirConfMixin.populate_dir`.
         """
-
-        path_is_ok = False
-        dirpath = Path(dirpath)
-        if dirpath.exists():
-            if dirpath.is_dir():
-                try:
-                    next(dirpath.iterdir())
-                except StopIteration:
-                    # empty dir
-                    path_is_ok = True
-                else:
-                    # nonempty dir
-                    if not force:
-                        raise RuntimeContextError(
-                                f"path {dirpath} is not empty. Set"
-                                f" force=True to proceed anyways")
-                    path_is_ok = True
-            else:
-                # not a dir
-                raise RuntimeContextError(
-                        f"path {dirpath} exists but is not a valid directory."
-                        )
-        else:
-            # non exists
-            path_is_ok = True
-        assert path_is_ok  # should not fail
-
-        def get_or_create_item_path(item, path, dry_run=False):
-            if path.exists():
-                cls.logger.debug(
-                    f"{'overwrite' if item in cls._backup_items else 'use'}"
-                    f" existing {item} {path}")
-            else:
-                with logit(cls.logger.debug, f"create {item} {path}"):
-                    if not dry_run:
-                        if item.endswith('dir'):
-                            path.mkdir(parents=True, exist_ok=False)
-                        elif item.endswith('file'):
-                            touch_file(path)
-                        else:
-                            raise ValueError(f"unknown {item}")
-
-        if create:
-            for item in cls._backup_items:
-                content_path = cls._get_content_path(dirpath, item)
-                if content_path.exists():
-                    if not overwrite:
-                        cls._create_backup(content_path)
-
-        for item in cls._file_contents.keys():
-            content_path = cls._get_content_path(dirpath, item)
-            if not create and not content_path.exists():
-                raise RuntimeContextError(
-                        f"unable to initialize pipeline runtime"
-                        f" from {dirpath}:"
-                        f" missing {item} {content_path}. Set"
-                        f" create=True to create missing items")
-            if create:
-                get_or_create_item_path(item, content_path)
-
+        dirpath = cls.populate_dir(dirpath, **kwargs)
         return cls(rootpath=dirpath)
 
     @classmethod
     def from_config(cls, *configs):
+        """
+        Create `RuntimeContext` instance from a set of configs.
+
+        This method allow constructing `RuntimeContext`
+        from multiple configuration dicts.
+
+        For a single config dict, use the constructor instead.
+
+        Parameters
+        ----------
+        *configs : tuple
+            The config dicts.
+        """
         cfg = deepcopy(configs[0])
+        # TODO maybe we nned to make deepcopy of all?
         for c in configs[1:]:
             rupdate(cfg, c)
         return cls(config=cfg)
 
     def symlink_to_bindir(self, src, link_name=None):
+        """Create a symbolic link of of `src` in :attr:`bindir`.
+
+        """
         src = Path(src)
         if link_name is None:
             link_name = src.name
@@ -341,10 +240,9 @@ class RuntimeContext(object):
         """Populate the setup file (50_setup.yaml).
 
         Parameters
-        ==========
+        ----------
         config : dict, optional
             Additional config to add to the setup file.
-
         overwrite : bool
             Set to True to force overwrite the existing
             setup info. Otherwise a `RuntimeContextError` is
@@ -358,9 +256,9 @@ class RuntimeContext(object):
                     self.logger.debug(
                         "runtime context is already setup, overwrite")
                 else:
-                    self.logger.debug("runtime context is already setup, skip")
-                    return
-
+                    raise RuntimeContextError(
+                            'runtime context is already setup'
+                            )
         if config is None:
             config = dict()
         else:
@@ -373,9 +271,11 @@ class RuntimeContext(object):
                     'created_at': Time.now().isot,
                     }
             })
-        # write the setup context to the config_file
+        # write the setup context to the setup_file
         with open(self.setup_file, 'w') as fo:
             yaml.dump(config, fo)
         # invalidate the config cache if needed
+        # so later self.config will pick up the new setting
         if 'config' in self.__dict__:
             del self.__dict__['config']
+        return self
---
+++ tolteca/utils/tests/test_runtime_context.py
@@ -0,0 +1,86 @@
+#! /usr/bin/env python
+
+import tempfile
+import pytest
+from pathlib import Path
+from tollan.utils.sys import touch_file
+from schema import SchemaMissingKeyError
+from copy import deepcopy
+from .. import RuntimeContext, RuntimeContextError
+from ... import version
+
+
+def test_runtime_context():
+
+    with tempfile.TemporaryDirectory() as tmp:
+        tmp = Path(tmp)
+        with pytest.raises(
+                RuntimeContextError, match='missing runtime context contents'
+                ):
+            rc = RuntimeContext(rootpath=tmp)
+        rc = RuntimeContext.from_dir(tmp, create=True)
+        assert rc.rootpath == tmp
+        assert rc.to_dict() == {
+                'rootpath': tmp,
+                'bindir': tmp / 'bin',
+                'caldir': tmp / 'cal',
+                'logdir': tmp / 'log',
+                'setup_file': tmp / '50_setup.yaml',
+                }
+        assert all(getattr(rc, f).exists() for f in [
+            'bindir', 'caldir', 'logdir', 'setup_file'])
+        with open(tmp / '00_a.yaml', 'w') as fa, \
+                open(tmp / '10_b.yaml', 'w') as fb:
+            fa.write('''
+---
+a: 1
+b:
+  c: 2
+''')
+            fb.write('''
+---
+b:
+  c: 1
+d: 'test'
+''')
+        touch_file(tmp / "not_a_config.yaml")
+        assert [f.name for f in rc.config_files] == \
+            ['00_a.yaml', '10_b.yaml', '50_setup.yaml']
+
+        with pytest.raises(
+                SchemaMissingKeyError,
+                match="Missing key: 'setup'"
+                ):
+            assert rc.config
+        # need setup
+        rc.setup()
+        assert rc.config
+        assert rc.config['setup']['version'] == version.version
+        # create directly
+        rc2 = RuntimeContext(tmp)
+        assert rc2.config == rc.config
+
+        # create from config
+        rc3 = RuntimeContext(config=rc.config)
+        assert rc3.config == rc.config
+        assert not rc3.is_persistent
+        assert rc3.rootpath == rc.rootpath == tmp
+
+        # create from config, but with runtime removed
+        cfg = deepcopy(rc.config)
+        cfg.pop('runtime')
+        rc4 = RuntimeContext(config=cfg)
+        assert rc4.config
+        assert rc4.config['runtime']['bindir'] is None
+        assert not rc4.is_persistent
+        assert rc4.rootpath is None
+
+        # create from config, but with setupremoved
+        cfg = deepcopy(rc.config)
+        cfg.pop('setup')
+        rc5 = RuntimeContext(config=cfg)
+        with pytest.raises(
+                SchemaMissingKeyError,
+                match="Missing key: 'setup'"
+                ):
+            assert rc5.config
