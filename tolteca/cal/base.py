2f57720b848c25796b11456afcb2890d95700db0
Jerry Ma <jerry.ma.nk@gmail.com>
2021-06-01 11:51:10 -0400

    add support for non-persistent calib object

--- tolteca/cal/base.py
+++ tolteca/cal/base.py
@@ -6,30 +6,46 @@ from pathlib import Path
 from astropy.io.misc.yaml import load as yaml_load
 from tollan.utils.schema import create_relpath_validator
 from schema import Schema
-from cached_property import cached_property
+# from cached_property import cached_property
 
 
 class CalibBase(object):
     """Base class for calibration object.
 
-    Calibration object (calobj) are typically static data that are generated
-    from external procedures. This class defines a scheme to access such data
-    via the notion of the "index file", which is created for each dataset and
-    describes all the details of the data items.
+    Calibration object (calobj) is typically static dataset that are generated
+    from external procedures. This class provides a common interface to access
+    such data via the notion of the "index file", which is a YAML file created
+    along with the data items in a calibration object, describing the details
+    of the contents.
 
     Parameters
     ----------
-    index_filepath : str, `pathlib.Path`
-
+    indexfile : str, `pathlib.Path`, optional
         The path of the index file.
+    index : dict, optional
+        The loaded index.
+    rootpath : str, `pathlib.Path`, optional
+        The rootpath to use when resolving relative paths in the index.
+        This has to be provided when creating from `index`.
     """
 
-    def __init__(self, index_filepath):
-        index_filepath = Path(index_filepath)
-        with open(index_filepath, 'r') as fo:
-            index = yaml_load(fo)
-        self._index = self.validate_index(index)
-        self._rootpath = index_filepath.parent
+    def __init__(self, indexfile=None, index=None, rootpath=None):
+        if sum([indexfile is None, index is None]) != 1:
+            raise ValueError("need one of indexfile or index.")
+        if indexfile is not None:
+            indexfile = Path(indexfile)
+            with open(indexfile, 'r') as fo:
+                index = yaml_load(fo)
+            if rootpath is None:
+                rootpath = indexfile.parent
+        elif index is not None:
+            if rootpath is None:
+                raise ValueError("need rootpath when index is given.")
+            pass
+        else:
+            raise  # should not happen
+        self._index = self._validate_index(index)
+        self._rootpath = rootpath
         self._path_validator = create_relpath_validator(self._rootpath)
 
     @property
@@ -37,13 +53,18 @@ class CalibBase(object):
         """The index of the calibration object."""
         return self._index
 
-    @classmethod
-    def validate_index(cls, index):
-        """The schema that validates the index.
+    @property
+    def rootpath(self):
+        """The rootpath to resolve paths in the index."""
+        return self._rootpath
 
-        The default is to not validate.
-        """
-        return index
+    @property
+    def _index_schema(self):
+        # default schema is arbituary dict
+        return Schema({object: object})
+
+    def _validate_index(self, index):
+        return self._index_schema.validate(index)
 
     @classmethod
     def from_uri(cls, uri, **kwargs):
@@ -60,9 +81,14 @@ class CalibBase(object):
         return m(dispatch_uri(uri), **kwargs)
 
     @classmethod
-    def from_indexfile(cls, index_filepath):
-        """Create instance from index filepath."""
-        return cls(index_filepath)
+    def from_indexfile(cls, indexfile):
+        """Create instance from index file."""
+        return cls(indexfile=indexfile)
+
+    @classmethod
+    def from_index(cls, index, rootpath):
+        """Create instance from index and rootpath."""
+        return cls(index=index, rootpath=rootpath)
 
     def resolve_path(self, path):
         """Return the absolute path by prefixing with :attr:`rootpath`."""
