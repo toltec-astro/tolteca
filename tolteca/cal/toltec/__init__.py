d49d122ca9ec7f6e101edaade9fe5772b5edd7f7
Jerry Ma <jerry.ma.nk@gmail.com>
2021-06-15 12:39:50 -0400

    fix toltec cal stack

--- tolteca/cal/toltec/__init__.py
+++ tolteca/cal/toltec/__init__.py
@@ -1,43 +1,67 @@
 #! /usr/bin/env python
 
 from astropy.table import Table, vstack
-from schema import Schema, Optional, Or
+from schema import Schema, Or, SchemaError
+from astropy.io.misc import yaml
+from astropy.utils.decorators import classproperty
+
+from tollan.utils.registry import Registry
+
 from ..base import CalibStack, CalibBase
+from ...common.toltec import info as toltec_info
+from ...common.toltec import info as lmt_info
+
+
+__all__ = ['ToltecCalibBase']
+
+
+_registry = Registry.create()
+"""A registry to hold TolTEC calibration object types."""
 
 
 class ToltecCalibBase(CalibBase):
-    """Base calss for calibration object for the TolTEC instrument.
+    """Base class for calibration object for the TolTEC instrument.
 
     """
 
-    # this defines all the unique keys that could be used
-    # in TolTEC calibration objects.
-    _unique_key_dict = {
-            'array_names': ['a1100', 'a1400', 'a2000']
-            'interfaces': [
-                'toltec{i}' for i in range(13)] \
-                + ['hwpr', 'toltec_hk', '']
+    info = {
+            'toltec': toltec_info,
+            'lmt': lmt_info
             }
+    """Assorted instrument info for TolTEC and LMT."""
 
-    array_names = ['a1100', 'a1400', 'a2000']
-    """The TolTEC array names.
+    array_names = info['toltec']['array_names']
+    """The TolTEC array names."""
 
-    These are used as the key to query per-array properties.
-    """
+    interfaces = info['toltec']['interfaces']
+    """The TolTEC instrument interface names."""
 
-    interfaces = ['toltec{}']
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        _registry_key = getattr(cls, '_registry_key', None)
+        if _registry_key is None:
+            return
+        _registry.register(_registry_key, cls)
+
+    @property
+    def data_index(self):
+        """The index under the ``cls._registry_key``."""
+        return self.index[self._registry_key]
 
 
 class ToltecArrayProp(ToltecCalibBase):
     """Calibration object for TolTEC array property table."""
 
-    def _index_schema(cls, index):
+    _registry_key = 'array_prop_table'
+
+    @classproperty
+    def _index_schema(cls):
         # apt can be specified per-array or merged in the index file.
         return Schema({
-            'array_prop_table': Or(
+            cls._registry_key: Or(
                 {
                     # one table per array
-                    str: {
+                    Or(cls.array_names): {
                         'path': str,
                         }
                     },
@@ -46,13 +70,33 @@ class ToltecArrayProp(ToltecCalibBase):
                     'path': str
                     },
                 ),
-            Optional(object): object
-            })
-        return s.validate(index)
+            }, ignore_extra_keys=True)
 
     def _is_merged_apt(self):
         # apt can be specified per-array or merged in the index file.
-        return 'path' in self.index['array_prop_table']
+        return 'path' in self.data_index
+
+    def _make_merged_apt(cls, tbls):
+        # strip the meta and attach array name and array index
+        # to the table
+        def _proc(t):
+            m = t.meta
+            t.meta = None
+            t['array_name'] = m['name']
+            t['array_name'].description = 'The array name.'
+            t['array'] = m['index']
+            t['array'].description = \
+                'The array index (0=a1100, 1=a1400, 2=a2000).'
+            return m
+
+        meta_list = [_proc(t) for t in tbls]
+
+        tbl = vstack(tbls, join_type='exact')
+        # merge the meta data to a dict keyed off with the array_name
+        for meta in meta_list:
+            tbl.meta[meta['name']] = meta
+        tbl.meta['array_names'] = [m['name'] for m in meta_list]
+        return tbl
 
     def get(self, array_name=None):
         """Return the array property table for `array_name`.
@@ -67,48 +111,41 @@ class ToltecArrayProp(ToltecCalibBase):
             # stack the tables
             if self._is_merged_apt():
                 filepath = self.resolve_path(
-                        self.index['array_prop_table']['path'])
+                        self.data_index['path'])
                 tbl = Table.read(filepath, format='ascii')
             else:
-                tbls = [
+                tbl = self._make_merged_apt([
                         self.get(array_name=n)
+                        # this uses the array_names from index to
+                        # allow apt with not all three arrays
                         for n in self.index['array_names']
-                        ]
-
-                # strip the meta and attach array name and array index
-                # to the table
-                def _proc(t):
-                    m = t.meta
-                    t.meta = None
-                    t['array_name'] = m['name']
-                    t['array_name'].description = 'The array name.'
-                    t['array'] = m['index']
-                    t['array'].description = \
-                        'The array index (0=a1100, 1=a1400, 2=a2000).'
-                    return m
-
-                metas = [_proc(t) for t in tbls]
-
-                tbl = vstack(tbls, join_type='exact')
-                # merge the meta data to a dict keyed off with the array_name
-                for meta in metas:
-                    tbl.meta[meta['name']] = meta
-                    tbl.meta['array_names'] = self.index['array_names']
+                        ])
             return tbl
         if self._is_merged_apt():
             tbl = self.get(array_name=None)
             return tbl[tbl['array_name'] == array_name]
         filepath = self.resolve_path(
-                self.index['array_prop_table'][array_name]['path'])
+                self.data_index[array_name]['path'])
         return Table.read(filepath, format='ascii')
 
 
-class ToltecPassband(CalibBase):
+class ToltecPassband(ToltecCalibBase):
     """Calibration object for TolTEC passband table."""
 
-    @property
-    def array_names(self):
-        return self.index['array_names']
+    _registry_key = 'passband_table'
+
+    @classproperty
+    def _index_schema(cls):
+        # apt can be specified per-array or merged in the index file.
+        return Schema({
+            cls._registry_key: {
+                # one table per array
+                array_name: {
+                    'path': str,
+                    }
+                for array_name in cls.array_names
+                },
+            }, ignore_extra_keys=True)
 
     def get(self, array_name=None):
         """Return the passband for `array_name`.
@@ -123,19 +160,36 @@ class ToltecPassband(CalibBase):
             # TODO add merged passband table
             raise NotImplementedError
         filepath = self.resolve_path(
-                self.index['passbands'][array_name]['path'])
+                self.data_index[array_name]['path'])
         return Table.read(filepath, format='ascii')
 
 
-class ToltecCalib(CalibStack):
+class ToltecCalib(ToltecCalibBase, CalibStack):
     """A class to manage TolTEC calibration data."""
 
-    @property
-    def array_names(self):
-        return self.index['array_prop_table']
-
     def get_array_prop_table(self, **kwargs):
-        return self.index['array_prop_table'].get(**kwrags)
+        return self.index['array_prop_table'].get(**kwargs)
 
-    def get_passband_table(self, **kwargs):
-        return self.index['passband_table'].get(array_name)
+    def get_passband(self, **kwargs):
+        return self.index['passband'].get(**kwargs)
+
+    @classmethod
+    def from_indexfile(cls, indexfile):
+        """Create calibration stack for TolTEC from index file.
+
+        The appropriate calibration object(s) will be created from the
+        specified index file by check the contents against the schema
+        of known subclasses of `ToltecCalibBase`.
+
+        """
+        with open(indexfile, 'r') as fo:
+            index = yaml.load(fo)
+
+        _index = dict()
+        for key, cls in _registry.items():
+            try:
+                calobj = cls(index=index)
+            except SchemaError:
+                continue
+            _index[key] = calobj
+        return cls(index=_index)
--- tolteca/simu/tests/test_toltec_lmt_atm_model.py
+++ tolteca/simu/tests/test_toltec_lmt_atm_model.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 
 
-from ..toltec.lmt_loading_models import LmtAtmosphereModel
+from ..toltec.lmt_loading_models import (
+        LmtAtmosphereModel, ArrayLoadingModel)
 import astropy.units as u
 import pytest
 
@@ -15,6 +16,18 @@ def test_lmt_atm_model():
         m25 = LmtAtmosphereModel(name='am_q25', alt=100 << u.deg)
     m25 = LmtAtmosphereModel(name='am_q25', alt=50 << u.deg)
     m50 = LmtAtmosphereModel(name='am_q50')
+    # m75 = LmtAtmosphereModel(name='am_q75')
 
-    print(m25)
-    print(m50)
+    print(m25(1.1 << u.mm))
+    print(m50(1.1 << u.mm))
+    # print(m75)
+
+
+def test_detector_loading_model():
+
+    alm_a1100 = ArrayLoadingModel(array_name='a1100', atm_model_name='am_q50')
+    alm_a1400 = ArrayLoadingModel(array_name='a1400', atm_model_name='am_q50')
+
+    print(alm_a1100(70. << u.deg))
+    print(alm_a1400(70. << u.deg))
+    # print(m75)
