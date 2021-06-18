470a3ee63fe6e6343fc613c397ea22606aa62efa
Jerry Ma <jerry.ma.nk@gmail.com>
2021-06-18 13:42:29 -0400

    Deployed 1c6dc6b with MkDocs version: 1.2.1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev84+gfc9946a.d20200423',
+    VERSION: '0.1.dev323+g1c6dc6b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
