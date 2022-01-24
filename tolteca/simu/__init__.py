582a656f61ee3449edad34be3ad912340b475b56
toltec <toltec@umass.edu>
2022-01-24 13:25:21 +0000

    fix source name in obs planner LMT OT

--- tolteca/simu/__init__.py
+++ tolteca/simu/__init__.py
@@ -347,6 +347,8 @@ def _sre_lmtot(rt):
     ot_lines.append(f"# LMT OT script created by tolteca.simu at {Time.now()}")
     # ot_lines.append(f'ObsGoal Dcs; Dcs -ObsGoal "{cfg["jobkey"]}"')
     ot_lines.append(f'ObsGoal Dcs; Dcs -ObsGoal Science')
+    source_name = cfg['mapping']['target']
+    source_name = source_name.split(' ').join(',')
     ref_coord = simobj.resolve_target(
             mapping.target,
             mapping.t0,
@@ -356,7 +358,7 @@ def _sre_lmtot(rt):
         " -DecProperMotionCor 0"
         " -Dec[0] {Dec} -Dec[1] {Dec}"
         " -El[0] 0.000000 -El[1] 0.000000 -EphemerisTrackOn 0 -Epoch 2000.0"
-        " -GoToZenith 0 -L[0] 0.0 -L[1] 0.0 -LineList [] -Planet None"
+        " -GoToZenith 1 -L[0] 0.0 -L[1] 0.0 -LineList [] -Planet None"
         " -RaProperMotionCor 0 -Ra[0] {RA} -Ra[1] {RA}"
         " -SourceName {Name} -VelSys Lsr -Velocity 0.000000 -Vmag 0.0".format(
             RA=ref_coord.ra.to_string(
