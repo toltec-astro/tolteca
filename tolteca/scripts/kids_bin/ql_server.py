7fa93af217cd1b50db9881667386143f3dbdd2e0
toltec <toltec@taco>
2023-04-01 02:33:34 +0000

    fix ql server on tcs tunes

--- tolteca/scripts/kids_bin/ql_server.py
+++ tolteca/scripts/kids_bin/ql_server.py
@@ -7,6 +7,7 @@ import yaml
 
 from tolteca.datamodels.toltec import BasicObsDataset
 from tolteca.datamodels.toltec.data_prod import ToltecDataProd, DataItemKind, ScienceDataProd
+from tollan.utils.log import get_logger, init_log
 
 
 QL_SEARCH_PATHS = [
@@ -53,38 +54,38 @@ def collect_from_citlali_index(index_file):
 
 
 def get_dp_for_dataset(rootdir, dataset, reduced_dir='toltec/reduced'):
+    logger = get_logger()
     reduced_dir = rootdir.joinpath(reduced_dir)
     obsnum = dataset[0]['obsnum']
     dpdir = reduced_dir.joinpath(f'{obsnum}')
+    if not dpdir.exists():
+        logger.debug(f"{dpdir=} does not exist, no data prod found for {obsnum=}")
+        return None
+
+    logger.debug(f"collect data prod for {obsnum=} {dataset=} {dpdir=}")
+
     context = {
             'dpdir': dpdir
             }
-    if not dpdir.exists():
-        dpdir = None
-    if dataset[0]['master_name'] == 'ics':
-        # toltec kids data
-        data_files = list(reduced_dir.glob(f'toltec[0-9]?_{obsnum:06d}_*'))
-        if not data_files:
-            return None
-        bods = BasicObsDataset.from_files(data_files)
-        bods.meta['context'] = context
-        return bods
-    if dataset[0]['master_name'] == 'tcs':
-        # reduction is on per obsnum directory basis
-        if dpdir is None:
-            return
-        citlali_index = dpdir.joinpath("index.yaml")
-        if citlali_index.exists():
-            # translate the index to dp index
-            dps = collect_from_citlali_index(citlali_index)
-        else:
-            dps = ToltecDataProd.collect_from_dir(dpdir)
-        if not dps:
-            return None
-        # get the one with largest id
-        dp = sorted(dps.index['data_items'], key=lambda d: d.meta['id'])[-1]
-        dp.meta['context'] = context
-        return dp
+    citlali_index = dpdir.joinpath("index.yaml")
+    if citlali_index.exists():
+        # translate the index to dp index
+        dps = collect_from_citlali_index(citlali_index)
+    else:
+        dps = ToltecDataProd.collect_from_dir(dpdir)
+    if not dps:
+        # this is for kids data only, for now return a dummy data prod
+        return ToltecDataProd(source={
+            'meta': {
+                'name': f'kids_ql_{obsnum}',
+                'context': context
+                },
+            'data_items': []
+            })
+    # get the one with largest redu id
+    dp = sorted(dps.index['data_items'], key=lambda d: d.meta['id'])[-1]
+    dp.meta['context'] = context
+    return dp
 
 
 def _vstack_images(files):
@@ -107,8 +108,8 @@ def _vstack_images(files):
 
 
 def get_quicklook_response(ql_files, save_path):
-
-    print(f"make summary for ql_files={ql_files}")
+    logger = get_logger()
+    logger.debug(f"make summary for ql_files={ql_files}")
     summary_image_path = Path(save_path).joinpath('lmt_tcs_quicklook_summary.png')
     if ql_files:
         summary_image = _vstack_images(ql_files)
@@ -124,59 +125,59 @@ def get_quicklook_response(ql_files, save_path):
 
 
 def get_quicklook_data(rootdir, bods, search_paths=None):
+    logger = get_logger()
     if search_paths is None:
         search_paths = ['toltec/reduced', ]
+    # search for the first dp avaiable dp in all search paths
     for rd in search_paths:
         dp = get_dp_for_dataset(rootdir, bods, reduced_dir=rd)
         if dp is not None:
+            logger.debug(f"found {dp!r} in {rd}")
             break
     # extract quick look data from dp
     if dp is None:
+        logger.debug(f"no dp found in {search_paths=}")
         return None, 'No reduced files found.'
+
+    # now compose the ql data object
     dpdir = dp.meta['context']['dpdir']
-    if isinstance(dp, BasicObsDataset):
-        # just report the number of reduced files
-        kids_ql_data = get_kids_ql_data(dp)
-        ql_response = get_quicklook_response(kids_ql_data['quicklook_files'], save_path=dpdir)
-        meta = {
-                'name': f'{dp!r}',
-                }
-        meta.update(ql_response)
-        return {
-                'meta': meta,
-                # 'n_data_items': len(dp),
-                'data_items': [
-                    {
-                        'filepath': entry['source']
-                        }
-                    for entry in dp.index_table
-                    ],
-                'kids_ql_data': get_kids_ql_data(dp),
-                }, None
-    if isinstance(dp, ToltecDataProd):
-        # handle pointing reader outputs
+    index = dp.index.copy()
+
+    # collect all ql data
+    ql_files = []
+
+    # kids data
+    kids_ql_data = index['kids_ql_data'] = get_kids_ql_data(dp)
+    ql_files.extend(kids_ql_data['quicklook_files'])
+
+    # citlali reduction
+    if len(index['data_items']) > 0:
+        # this is citlali reduction, check ql by obs goal
         obs_goal = dr.get_obs_goal(bods)
-        index = dp.index.copy()
-        print(f'obs goal was {obs_goal}')
-        ql_files = []
+        logger.debug(f' collect ql files for {obs_goal=}')
         if obs_goal in ['test', 'pointing'] or 'pointing' in index['data_items'][0]['filepath'].name:
             d = pointing_reader_data = get_pointing_reader_data(dp)
             index['pointing_reader_data'] = pointing_reader_data
         elif obs_goal in ['beammap', ] or 'beammap' in index['data_items'][0]['filepath'].name:
             d = beammap_reader_data = get_beammap_reader_data(dp)
             index['beammap_reader_data'] = beammap_reader_data
-        for item in d['data_items']:
-            ql_files.extend(item['quicklook_files'])
+        else:
+            d = None
+        logger.debug(f"ql_data: {d}")
+        if d is not None:
+            for item in d['data_items']:
+                ql_files.extend(item['quicklook_files'])
+    if len(ql_files) > 0:
         ql_response = get_quicklook_response(ql_files, save_path=dpdir)
         index['meta'].update(ql_response)
-        print(index)
         return index, None
     return None, "Unkown type of data product."
 
 
 def get_kids_ql_data(dp):
-    print(f'collecting kids ql data for {dp}')
+    logger = get_logger()
     dpdir = dp.meta['context']['dpdir']
+    logger.debug(f'collecting kids ql data for {dp} {dpdir=}')
     quicklook_files = []
     if dpdir is not None:
         quicklook_files += list(dpdir.glob("ql_*.png"))
@@ -187,7 +188,8 @@ def get_kids_ql_data(dp):
 
 
 def get_pointing_reader_data(dp):
-    print(f'collecting pointing reader data for {dp}')
+    logger = get_logger()
+    logger.debug(f'collecting pointing reader data for {dp}')
     # get the data rootpath
     datadir = Path(dp.index['data_items'][0]['filepath']).parent
     result = {
@@ -214,7 +216,8 @@ def get_pointing_reader_data(dp):
 
 
 def get_beammap_reader_data(dp):
-    print(f'collecting beammap reader data for {dp}')
+    logger = get_logger()
+    logger.debug(f'collecting beammap reader data for {dp}')
     # get the data rootpath
     datadir = Path(dp.index['data_items'][0]['filepath']).parent
     result = {
@@ -276,7 +279,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         ql_data, message = get_quicklook_data(rootdir, bods, search_paths=QL_SEARCH_PATHS)
         if ql_data is None:
             return {
-                    'exit_code': 1,
+                    'exit_code': -1,
                     'message': message
                     }
         return {
@@ -297,6 +300,7 @@ def run_server():
 
 if __name__ == "__main__":
     import sys
+    init_log(level='DEBUG')
 
     if len(sys.argv) > 1:
         resp = MyTCPHandler.handle_obsnum(int(sys.argv[1]))
