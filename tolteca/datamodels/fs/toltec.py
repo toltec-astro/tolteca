004a8bcced7d0093700f1f09ade172d5228cc625
toltec <toltec@taco>
2023-04-02 07:41:18 +0000

    exclude tel_toltec2

--- tolteca/datamodels/fs/toltec.py
+++ tolteca/datamodels/fs/toltec.py
@@ -253,7 +253,7 @@ def _meta_from_lmt_tel_filename(file_loc):
         'scannum': int,
         'ut': parse_ut,
         'fileext': lambda s: s.lower(),
-        'interface': lambda s: ('lmt' if s == 'tel' or s.startswith('tel_') else s)
+        'interface': lambda s: ('lmt' if s in ['tel', 'tel_toltec'] else 'tel2')
         }
 
     meta = dict_from_regex_match(
