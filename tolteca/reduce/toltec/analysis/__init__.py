ee5a6c55a8c393dd41fae627e3e51c3c4a386ec0
Jerry <jerry.ma.nk@gmail.com>
2026-03-02 22:56:42 -0500

    add timestream projection mode

--- tolteca/reduce/toltec/analysis/__init__.py
+++ tolteca/reduce/toltec/analysis/__init__.py
@@ -2,4 +2,5 @@
 
 # from . import photutils  # noqa: F401
 from . import minkasi  # noqa: F401
-from . import simu # noqa: F401
+from . import simu  # noqa: F401
+from . import timestream_project  # noqa: F401
